fbb0e09c7cac3de6214ecae51ddf0ba6eacdff6d
NgigiN <ngigi.nyongo@gmail.com>
2023-12-28 15:01:40 +0300

    completed test 0

---
+++ markdown2html.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Parsing Markdown to HTML
+"""
+if __name__ == "__main__":
+    import sys
+    from os import path
+
+if len(sys.argv) < 3:
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+    exit(1)
+
+if not path.exists(sys.argv[1]) or not str(sys.argv[1]).endswith(".md"):
+    sys.stderr.write("Missing " + sys.argv[1] + "\n")
+    exit(1)
+
+exit(0)
